2352dbc6b0032fa9ecee07553ed54ac0f39ec4db
jpfMing21 <jpg@mingoville.com>
2013-04-15 01:59:18 +0200

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialDashboard::Application.config.secret_token = '310189f161c1da2462c036ba50552358b0b6ab05686a34813065b13dacc0b740c3bba4561da3c85b7814d96a5666940f2c8359ba3c95e7f416616b60f50f4961'
